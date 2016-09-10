d14fa271728e36e6d3eaf480b3b2968b0679a404
badwi <slamet.badwi@gmail.com>
2016-09-11 06:28:28 +0700

    Modul 2 submission

--- app/src/main/java/id/my/badwi/hitungluas/MainActivity.java
+++ app/src/main/java/id/my/badwi/hitungluas/MainActivity.java
@@ -1,36 +1,69 @@
 package id.my.badwi.hitungluas;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
+//import android.text.TextUtils;
+//import android.view.View;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+//import android.widget.EditText;
+//import android.widget.TextView;
+//import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText edtPanjang, edtLebar;
-    private Button btnHitung;
-    private TextView txtLuas;
+//    private EditText edtPanjang, edtLebar;
+//    private Button btnHitung;
+//    private TextView txtLuas;
+    private Button btnSub1, btnSub2, btnDial;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getSupportActionBar().setTitle("Hitung Luas Persegi Panjang");
-        edtPanjang = (EditText)findViewById(R.id.edt_panjang);
-        edtLebar = (EditText)findViewById(R.id.edt_lebar);
-        btnHitung = (Button)findViewById(R.id.btn_hitung);
-        txtLuas = (TextView)findViewById(R.id.txt_luas);
+        getSupportActionBar().setTitle("Sample Intent");
 
-        btnHitung.setOnClickListener(new View.OnClickListener() {
+        btnSub1 = (Button)findViewById(R.id.btn_activity_sub_1);
+        btnSub2 = (Button)findViewById(R.id.btn_activity_sub_2);
+        btnDial = (Button)findViewById(R.id.btn_activity_sub_3);
+
+        btnSub1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String panjang = edtPanjang.getText().toString().trim();
-                String lebar = edtLebar.getText().toString().trim();
+                Intent intent = new Intent(MainActivity.this, Sub1Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSub2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Sub2Activity.class);
+                intent.putExtra(Sub2Activity.KEY_DATA, "Training Aplikasi Android");
+                startActivityForResult(intent, 0);
+            }
+        });
+
+        btnDial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:082121212093"));
+                startActivity(intent);
+            }
+        });
+//        edtPanjang = (EditText)findViewById(R.id.edt_panjang);
+//        edtLebar = (EditText)findViewById(R.id.edt_lebar);
+//        btnHitung = (Button)findViewById(R.id.btn_hitung);
+//        txtLuas = (TextView)findViewById(R.id.txt_luas);
+//
+//        btnHitung.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String panjang = edtPanjang.getText().toString().trim();
+//                String lebar = edtLebar.getText().toString().trim();
 
 //                if (edtPanjang.getText().toString().length() == 0 ||
 //                        edtLebar.getText().toString().length() == 0) {
@@ -43,22 +76,22 @@ public class MainActivity extends AppCompatActivity {
 //                else
 //
 //                {
-                try {
-                    double p = Double.parseDouble(panjang);
-                    double l = Double.parseDouble(lebar);
-
-                    double luas = p * l;
-
-                    txtLuas.setText("Luas: " + luas);
-                }
-                catch (NumberFormatException e) {
-
-                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
-                    txtLuas.setText("Luas: NOL");
-                }
-
+//                try {
+//                    double p = Double.parseDouble(panjang);
+//                    double l = Double.parseDouble(lebar);
+//
+//                    double luas = p * l;
+//
+//                    txtLuas.setText("Luas: " + luas);
 //                }
-            }
-        });
+//                catch (NumberFormatException e) {
+//
+//                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+//                    txtLuas.setText("Luas: NOL");
+//                }
+//
+////                }
+//            }
+//        });
     }
 }
